cb1ab51e49754b9d2877f389d3ba6a912b235401
Andrés Court <andres@andrescourt.com>
2023-02-13 23:27:46 -0500

    Feature: Added navbar and footer

---
+++ src/pages/403.tsx
@@ -0,0 +1,11 @@
+export default function Error404() {
+    return (
+        <div className="m-4">
+            <h1 className="text-4xl">
+                <span className="text-red-600">403</span>
+                Unauthorized
+            </h1>
+            <p className="">You are unauthorized to use this resource</p>
+        </div>
+    )
+}
---
+++ src/pages/admin/users.tsx
@@ -0,0 +1,10 @@
+export default function Users() {
+	return (
+		<div className='m-4'>
+			<h1 className='text-2xl text-indigo-700'>Protected Page</h1>
+			<p className='text-green-400'>
+				This is a protected page and should show only after login
+			</p>
+		</div>
+	);
+}
