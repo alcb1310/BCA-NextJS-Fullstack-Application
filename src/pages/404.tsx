8e612f5460281bc2f4db89623723857128510423
Andrés Court <andres@andrescourt.com>
2023-02-13 23:06:51 -0500

    NextJS configuration with tailwind and a 404 error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+export default function Error404() {
+    return (
+        <div className="m-4">
+            <h1 className="text-4xl">
+                <span className="text-red-600">404</span>
+                File Not Found
+            </h1>
+            <p className="">Unable to load the resource</p>
+        </div>
+    )
+}
